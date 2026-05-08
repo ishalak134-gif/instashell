e00e2c4ed67d61abe8f85eca2f060e4610a9088a
ishalak134-gif <282979691+ishalak134-gif@users.noreply.github.com>
2026-05-08 22:50:18 +0100

    Change author and Instagram details in install.sh
    Updated author and Instagram information in install.sh.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Author: github.com/thelinuxchoice
-#Instagram: @thelinuxchoice
+#Author: github.com/marwa__jalaal
+#Instagram: @marwa__jalaal
 trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
 
 checkroot() {
